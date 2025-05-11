19be83049f035d3007aeab89621f560f0c27f5ec
sabbircse499 <sabbir472003@gmail.com>
2025-05-12 02:48:25 +0600

    initial version

--- lib/player/draweview.dart
+++ lib/player/draweview.dart
@@ -27,20 +27,27 @@ class _SideState extends State<drawerview> {
             children: [
 
               Padding(
-                padding: const EdgeInsets.only(left: 8.0),
+                padding: const EdgeInsets.only(left: 40.0),
                 child: Row(
                   children: [
                     IconButton(
                       icon: Icon(Icons.menu,color: Colors.black,),
                       onPressed: () {
-                        Scaffold.of(context).openDrawer();
+                        Scaffold.of(context).closeDrawer();
                       },
                     ),
 
-                    SizedBox(width: 40,),
+                    SizedBox(width: 30,),
 
-                    Image.network('https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/466403359_578527141341423_3069202312520923209_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=AgjrIDjWpiYQ7kNvwHM3HE6&_nc_oc=Adl4d7iG1sHu3fkhYT_5dRg_EmpcHOYugQXtP73I0obKwa6wNUENok0SCupizpTPJqc&_nc_zt=23&_nc_ht=scontent.fdac41-1.fna&_nc_gid=Zn3LFsQy_z8aiWtVYm0oIQ&oh=00_AfI6t-iDBi0pnNTvl26KEdRuWfP_fCAi6p1joBYSv2cXyA&oe=6824FE2C',height: 40,width: 40,),
-                    Text("NafsTube"),
+                   InkWell(
+                     onTap: () {
+
+                     },
+                     child: Row(children: [
+                       Image.network('https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/466403359_578527141341423_3069202312520923209_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=AgjrIDjWpiYQ7kNvwHM3HE6&_nc_oc=Adl4d7iG1sHu3fkhYT_5dRg_EmpcHOYugQXtP73I0obKwa6wNUENok0SCupizpTPJqc&_nc_zt=23&_nc_ht=scontent.fdac41-1.fna&_nc_gid=Zn3LFsQy_z8aiWtVYm0oIQ&oh=00_AfI6t-iDBi0pnNTvl26KEdRuWfP_fCAi6p1joBYSv2cXyA&oe=6824FE2C',height: 30,width: 30,),
+                       Text("NafsTube"),
+                     ],),
+                   )
 
                   ],
                 ),
